14b2fe1b66dd5fc785ce699b378c06c6a60eb806
nyakiomaina <carolinenyakio21@gmail.com>
2024-02-25 18:11:29 +0300

    updating log msgs

--- src/App.tsx
+++ src/App.tsx
@@ -37,39 +37,41 @@ const App: React.FC = () => {
   }, []);
 
   async function prepareAndSendTransaction(signerAddress: string, chainId: string) {
-    // dummy data
-    const namespace = new Uint8Array([0x01, 0x02, 0x03, 0x04]);
-    const data = new TextEncoder().encode("Hello, Celestia!");
-    const shareVersion = 1;
-
-    const blobResult = pay_blobs(signerAddress, namespace, data, shareVersion);
-    const txBodyBytes = message_to_tx(blobResult);
-
-    const client = await StargateClient.connect("https://rpc.celestia.org");
-    const account = await client.getAccount(signerAddress);
-
-    if (!account) {
-      console.error("Account not found");
-      return;
+    try {
+      const namespace = new Uint8Array([0x01, 0x02, 0x03, 0x04]);
+      const data = new TextEncoder().encode("Hello, Celestia!");
+      const shareVersion = 1;
+  
+      console.log('Calling pay_blobs with:', { signerAddress, namespace, data, shareVersion });
+      const blobResult = pay_blobs(signerAddress, namespace, data, shareVersion);
+      console.log('Result from pay_blobs:', blobResult);
+  
+      const txBodyBytes = message_to_tx(blobResult);
+      console.log('Result from message_to_tx:', txBodyBytes);
+  
+      const client = await StargateClient.connect("https://rpc.celestia.org");
+      const account = await client.getAccount(signerAddress);
+      if (!account) {
+        console.error("Account not found");
+        return;
+      }
+  
+      const dummyPublicKey = "A1b2C3d4E5f6g7H8I9j0K1l2M3n4O5p6Q7r8S9t0U1v2W3x4Y5z6a7B8C9d0E1f2==";
+      const authInfoBytes = auth_info_encode(
+        dummyPublicKey,
+        BigInt(account.sequence),
+        "CELESTIA",
+        "1000",
+        BigInt(200000),
+        signerAddress,
+        signerAddress
+      );
+      console.log('Result from auth_info_encode:', authInfoBytes);
+    } catch (error) {
+      console.error('Error during transaction preparation:', error);
     }
-
-    const dummyPublicKey = "A1b2C3d4E5f6g7H8I9j0K1l2M3n4O5p6Q7r8S9t0U1v2W3x4Y5z6a7B8C9d0E1f2==";
-
-    const authInfoBytes = auth_info_encode(
-      dummyPublicKey,
-      BigInt(account.sequence),
-      "CELESTIA",
-      "1000",
-      BigInt(200000),
-      signerAddress,
-      signerAddress
-    );
-    const txBodyHex = Buffer.from(txBodyBytes).toString('hex');
-    const authInfoHex = Buffer.from(authInfoBytes).toString('hex');
-
-    console.log('Prepared txBodyBytes:', txBodyHex);
-    console.log('Prepared authInfoBytes:', authInfoHex);
   }
+  
 
   return (
     <div className="App">
